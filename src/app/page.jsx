b3dea7dd2dd579c9f2d146690e5f6c422194709d
Olushola <ememobongumoren2@gmail.com>
2025-04-15 17:54:07 +0100

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,31 @@
 import Image from "next/image";
-import dashImg from "@/public/images/header_performaceImg.png"
+import dashImg from "@/public/images/header_performaceImg.png";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex w-full h-[700px]">
-        <div className="w-1/2 bg-white">
-          <div>
-            <h2 className="text-[#323F73]">
-              Your Next Performance Management Platform
-            </h2>
-            <p className="text-[#95AFBA]">
-              Fully Customizable performance management platform that suits your
-              culture. not the other way around
-            </p>
+      <div className="h-dvh">
+        <div className="flex w-full h-full ">
+          <div className="w-1/2 bg-white flex flex-col gap-8 justify-center items-center">
+            <div className="p-28">
+              <h2 className="text-bluish-purple font-bold leading-16 text-5xl">
+                Your Next Performance Management Platform
+              </h2>
+              <p className="text-bluish-grey mt-8">
+                Fully Customizable performance management platform that suits
+                your culture. not the other way around
+              </p>
+            </div>
+            <div className="flex">
+              
+
+            </div>
           </div>
-        </div>
-        <div className="w-1/2 bg-[#EDEFF5] flex justify-end items-end">
-        <div>
-          <Image src={dashImg} alt="img" />
-        </div>
+          <div className="w-1/2 bg-light-ash flex justify-end items-end">
+            <div>
+              <Image src={dashImg} className="w-[100%]" alt="img" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
