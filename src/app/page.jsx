771b53e61cff6da59ba6a9c8199dc87b36cfa49a
Olushola <ememobongumoren2@gmail.com>
2025-04-16 15:04:37 +0100

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import dashImg from "@/public/images/header_performaceImg.png";
 import headerShow from "@/public/images/header-showBrand.png";
 import dash1 from "@/public/images/dash1.png";
-import goal_manage from "@/public/images/goal_manage.png";
-import performace_rev from "@/public/images/performace-rev.png";
-import remoter_tool from "@/public/images/remoter-tool.png";
-import dash1 from "@/public/images/dash1.png";
+import goal_manage from "../../public/images/goal-manage.png";
+import performance_rev from "../../public/images/performance-rev.png";
+import remoter_tool from "../../public/images/remoter-tool.png";
 import mailIcon from "@/public/icons/mail.svg";
+import align from "@/public/icons/align.svg";
+import remote from "@/public/icons/remote.svg";
 
 export default function Home() {
   return (
@@ -53,29 +54,41 @@ export default function Home() {
         </div>
       </div>
       {/* SECOND SECTION */}
-      <div className="flex">
+      <div className="flex gap-10">
         <div>
           <Image src={dash1} alt="dash" />
         </div>
-        <div>
-          <h3>Meet Our Products</h3>
-          <p className="w-1/2">
+        <div className="min-[1400px]:pr-28 pr-10">
+          <h3 className="text-3xl font-bold text-bluish-purple">Meet Our Products</h3>
+          <p className="w-[30%] text-bluish-grey mt-8">
             Our Products work Togather seamlessly to form talent science. start
             with one or more.{" "}
           </p>
           <div>
-            <div className="flex">
-              <span>
-                <Image src={goal_manage} alt="goal management" />
-              </span>
+            <div className="flex items-stretch ">
               <div>
-                <div className="flex flex-col gap-4 justify-center items-center bg-orange/5 text-[#E95829]">
+                <Image src={goal_manage} className="w-[90%] h-auto" alt="goal management" />
+              </div>
+              <div className="flex flex-col gap-5">
+                <div className="flex w-40 p-3 rounded-lg gap-4 justify-center items-center bg-orange/15 text-[#E95829]">
+                  <span>
+                    <Image src={align} alt="icon" />
+                  </span>
                   Align
                 </div>
+                <h2 className="text-2xl text-bluish-purple ">Goal Management</h2>
+                <p className="text-bluish-grey">
+                Our Products work Together seamlessly to form talent science. start with one or more. 
+                </p>
+                <div className="flex gap-14 text-bluish-purple items-center">
+                  <p>Daily Track</p>
+                  <p>Road Map</p>
+                </div>
+                <button className="flex mt-10 font-semibold justify-center items-center py-3 rounded-md px-3 w-40 bg-yellow text-[#323F73]">
+                  Learn more
+                </button>
               </div>
-
             </div>
-
           </div>
         </div>
       </div>
